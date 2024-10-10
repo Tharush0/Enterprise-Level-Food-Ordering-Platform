a5d1a8b0d190a7c0169e1d42b044dc517a52aca8
Tharush0 <tharushnawanjana70@gmail.com>
2024-10-11 01:13:52 +0530

    Create Mobile Navigation Bar

--- front-end/src/components/Header.tsx
+++ front-end/src/components/Header.tsx
@@ -1,14 +1,18 @@
 import { Link } from "react-router-dom";
+import MobileNav from "./MobileNav";
 
 const Header = () => { 
     return (
-        <div className="border-b-2 border-b-orange-500 py-6">
+        <div className="border-b-2 border-b-green-500 py-6">
             <div className="container mx-auto flex justify-between items-center">
                 <Link
                     to="/"
-                    className="text-3xl font-bold tracking-tight text-orange-500">
+                    className="text-3xl font-bold tracking-tight text-green-500 hover:text-green-400">
                     BestFoods.LK
                 </Link>
+                <div className="md:hidden"> 
+                    <MobileNav />
+                </div>
             </div>
      </div>
  )
