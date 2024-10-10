c9cfcd34f79a3f531dd555d12324f3a100e55f08
Tharush0 <tharushnawanjana70@gmail.com>
2024-10-11 01:27:38 +0530

    Create MainNav.tsx

---
+++ front-end/src/components/MainNav.tsx
@@ -0,0 +1,10 @@
+import { Button } from "./ui/button"
+
+const MainNav = () => { 
+    return (
+        <Button variant="ghost" className="font-bold bg-green-500 hover:bg-green-400 hover:text-white">
+            Log In
+        </Button>
+    )
+}
+export default MainNav;
